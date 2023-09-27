ddc424021e828a797e0605d3b552ce119ec26fa2
Sanjeev <sanjeev.p3269@gmail.com>
2023-09-27 13:23:27 +0530

    Initial commit

---
+++ website/static/index.js
@@ -0,0 +1,8 @@
+function deleteNote(noteItem){
+    fetch('/delete-note', {
+        method: 'POST',
+        body: JSON.stringify({ noteId: noteId })
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
